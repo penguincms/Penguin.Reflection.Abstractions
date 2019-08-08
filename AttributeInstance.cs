d711bd47e0146962ac345a96dd66cc4b7b2e5705
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 09:52:12 -0700

    initial check in

---
+++ AttributeInstance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace Penguin.Reflection.Abstractions
+{
+    /// <summary>
+    /// An instance of an attribute
+    /// </summary>
+    public class AttributeInstance
+    {
+        #region Properties
+
+        /// <summary>
+        /// The type/property that this was declared on
+        /// </summary>
+        public MemberInfo DeclaringMember { get; set; }
+
+        /// <summary>
+        /// And instantiated version of the attribute
+        /// </summary>
+        public Attribute Instance { get; set; }
+
+        /// <summary>
+        /// Is this attribute defined on a parent type/overridden property from where it was retrieved?
+        /// </summary>
+        public bool IsInherited { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a new instance
+        /// </summary>
+        /// <param name="declaringMember">Where the attribute was declared</param>
+        /// <param name="instance">An instance of this attribute</param>
+        /// <param name="isInherited">Is this attribute defined on a parent type/overridden property from where it was retrieved?</param>
+        public AttributeInstance(MemberInfo declaringMember, Attribute instance, bool isInherited)
+        {
+            if (instance is null)
+            {
+                throw new Exception("Can not instantiate null attribute instance");
+            }
+
+            DeclaringMember = declaringMember;
+            Instance = instance;
+            IsInherited = isInherited;
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ CoreType.cs
@@ -0,0 +1,38 @@
+﻿namespace Penguin.Reflection.Abstractions
+{
+    /// <summary>
+    /// The simplest usable unit of a type check
+    /// </summary>
+    public enum CoreType
+    {
+        /// <summary>
+        /// The type is null and cant be resolved
+        /// </summary>
+        Null,
+
+        /// <summary>
+        /// Any Collection, Array, List, etc.
+        /// </summary>
+        Collection,
+
+        /// <summary>
+        /// Any kind of dictionary
+        /// </summary>
+        Dictionary,
+
+        /// <summary>
+        /// A value type or anything that could easily be serialized into a single field
+        /// </summary>
+        Value,
+
+        /// <summary>
+        /// Any type that contains sub-properties and is best shown as a tree of values
+        /// </summary>
+        Reference,
+
+        /// <summary>
+        /// An enumeration or selection type
+        /// </summary>
+        Enum
+    }
+}
